a4d89d84c95f4f0232c82b6881c8ce74dc329dc9
Francisco Lopez <pancholopez@gmail.com>
2019-01-13 22:52:46 +0100

    simplify retry read and write

--- Toolkit/FileManagement/RetryFileStreamCopyService.cs
+++ Toolkit/FileManagement/RetryFileStreamCopyService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using FileManagement.Core;
 
@@ -7,35 +8,15 @@ namespace FileManagement
 {
     public class RetryFileStreamCopyService : IFileCopyService
     {
-        private async Task<int> ReadRetryAsync(Stream input, byte[] buffer, Stream output)
-        {
-            int totalBytesRead = 0;
-            int bytesRead;
-
-            do
-            {
-                //this can fail, so retry
-                bytesRead = await input.ReadAsync(buffer, 0, buffer.Length).ConfigureAwait(false);
-
-                totalBytesRead += bytesRead;
-
-                await WriteRetryAsync(output, buffer, bytesRead);
-            } while (bytesRead > 0);
-
-            return totalBytesRead;
-        }
-
-        private async Task WriteRetryAsync(Stream output, byte[] buffer, int bytesToWrite)
+        private static async Task<int> RetryAsync(Func<ConfiguredTaskAwaitable<int>> operation)
         {
             var retryCount = 0;
             var retryLimit = 3;
-            var retry = true;
-            while (retry)
+            while (true)
             {
                 try
                 {
-                    await output.WriteAsync(buffer, 0, bytesToWrite).ConfigureAwait(false);
-                    retry = false;
+                    return await operation();
                 }
                 catch (Exception exception)
                 {
@@ -46,38 +27,81 @@ namespace FileManagement
                 finally
                 {
                     if (retryCount > retryLimit)
-                    {
-                        retry = false;
-                        //trow retryException
-                    }
+                        throw new RetryLimitException("Retry file read limit reached.");
                 }
-            }//end retryWrite
+            }
+        }
+
+        private static async Task<int> ReadRetryAsync(Stream input, byte[] buffer)
+        {
+            var retryCount = 0;
+            var retryLimit = 3;
+            while (true)
+            {
+                try
+                {
+                    return await input.ReadAsync(buffer, 0, buffer.Length).ConfigureAwait(false);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception);
+                    retryCount++;
+                    await Task.Delay(1000);
+                }
+                finally
+                {
+                    if (retryCount > retryLimit)
+                        throw new RetryLimitException("Retry file read limit reached.");
+                }
+            }
+        }
+
+        private static async Task<int> WriteRetryAsync(Stream output, byte[] buffer, int bytesToWrite)
+        {
+            var retryCount = 0;
+            var retryLimit = 3;
+            while (true)
+            {
+                try
+                {
+                    await output.WriteAsync(buffer, 0, bytesToWrite).ConfigureAwait(false);
+                    return bytesToWrite;
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception);
+                    retryCount++;
+                    await Task.Delay(1000);
+                }
+                finally
+                {
+                    if (retryCount > retryLimit)
+                        throw new RetryLimitException("Retry file write limit reached.");
+                }
+            }
         }
 
         public async Task<IFileCopySummary> CopyAsync(FileItem source, FileItem destination)
         {
-            var buffer = new byte[32 * 1024];   //4k minimum
+            var buffer = new byte[32 * 1024];   //4k minimum - 128k recommended
             var input = new FileStream(source.FilePath, FileMode.Open, FileAccess.Read);
             var output = new FileStream(destination.FilePath, FileMode.Create, FileAccess.Write);
-            var totalBytesRead = 0;
-            var retryCount = 0;
-            var retryLimit = 3;
+            var totalBytesWritten = 0;
             try
             {
-                totalBytesRead = await ReadRetryAsync(input, buffer, output);
-            }
-            catch (Exception exception)
-            {
-                //if there is a write exception abort
-                //otherwise increment readRetry counter
-                Console.WriteLine(exception);
+                int bytesRead;
+                do
+                {
+                    bytesRead = await ReadRetryAsync(input, buffer);
+                    totalBytesWritten += await WriteRetryAsync(output, buffer, bytesRead);
+                } while (bytesRead > 0);
             }
             finally
             {
                 input.Close();
                 output.Close();
             }
-            return FileCopySummary.Create(source, destination, totalBytesRead);
+            return FileCopySummary.Create(source, destination, totalBytesWritten);
         }
     }
 }
