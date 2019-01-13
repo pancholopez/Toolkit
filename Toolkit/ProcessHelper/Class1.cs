d17596101b5ff71f1695950d4156be3c6ae0b949
Francisco Lopez <pancholopez@gmail.com>
2019-01-13 17:50:03 +0100

    process helper

---
+++ Toolkit/ProcessHelper/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+using SlowProcess;
+
+namespace ProcessHelper
+{
+    public class Class1
+    {
+        private BackgroundWorker _backgroundWorker;
+        public Class1()
+        {
+            InitializeBackgroundWorker();
+        }
+
+        private void InitializeBackgroundWorker()
+        {
+            _backgroundWorker = new BackgroundWorker();
+            _backgroundWorker.DoWork += OnDoWork;
+            _backgroundWorker.RunWorkerCompleted += OnRunWorkerCompleted;
+
+            _backgroundWorker.WorkerReportsProgress = true;
+            _backgroundWorker.ProgressChanged += _OnProgressChanged;
+        }
+
+        private void _OnProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnRunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            int iterations = (int) e.Result;
+        }
+
+        private void OnDoWork(object sender, DoWorkEventArgs e)
+        {
+            int result = 0;
+            int iterations = (int)e.Argument;
+
+            LongRunningProcess slowProcess = new LongRunningProcess(iterations);
+            foreach (var current in slowProcess)
+            {
+                result = current;
+            }
+
+            e.Result = result;
+        }
+    }
+}
---
+++ Toolkit/SlowProcess/LongRunningProcess.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace SlowProcess
+{
+    public class LongRunningProcess:IEnumerable<int>
+    {
+        private int _currentPosition;
+        private readonly int _totalIterations;
+
+        public LongRunningProcess(int iterations)
+        {
+            _totalIterations = iterations;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            _currentPosition = 0;
+            while (_currentPosition<_totalIterations)
+            {
+                Thread.Sleep(100);
+                _currentPosition++;
+                yield return _currentPosition;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
