8a0a9ee056058009200507e725c6e9351120a6b3
Francisco Lopez <pancholopez@gmail.com>
2019-01-13 23:14:56 +0100

    simplify retry method

--- Toolkit/FileManagement/RetryFileStreamCopyService.cs
+++ Toolkit/FileManagement/RetryFileStreamCopyService.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using FileManagement.Core;
 
@@ -8,7 +7,7 @@ namespace FileManagement
 {
     public class RetryFileStreamCopyService : IFileCopyService
     {
-        private static async Task<int> RetryAsync(Func<ConfiguredTaskAwaitable<int>> operation)
+        private static async Task<int> RetryAsync(Func<Task<int>> operation)
         {
             var retryCount = 0;
             var retryLimit = 3;
@@ -27,56 +26,7 @@ namespace FileManagement
                 finally
                 {
                     if (retryCount > retryLimit)
-                        throw new RetryLimitException("Retry file read limit reached.");
-                }
-            }
-        }
-
-        private static async Task<int> ReadRetryAsync(Stream input, byte[] buffer)
-        {
-            var retryCount = 0;
-            var retryLimit = 3;
-            while (true)
-            {
-                try
-                {
-                    return await input.ReadAsync(buffer, 0, buffer.Length).ConfigureAwait(false);
-                }
-                catch (Exception exception)
-                {
-                    Console.WriteLine(exception);
-                    retryCount++;
-                    await Task.Delay(1000);
-                }
-                finally
-                {
-                    if (retryCount > retryLimit)
-                        throw new RetryLimitException("Retry file read limit reached.");
-                }
-            }
-        }
-
-        private static async Task<int> WriteRetryAsync(Stream output, byte[] buffer, int bytesToWrite)
-        {
-            var retryCount = 0;
-            var retryLimit = 3;
-            while (true)
-            {
-                try
-                {
-                    await output.WriteAsync(buffer, 0, bytesToWrite).ConfigureAwait(false);
-                    return bytesToWrite;
-                }
-                catch (Exception exception)
-                {
-                    Console.WriteLine(exception);
-                    retryCount++;
-                    await Task.Delay(1000);
-                }
-                finally
-                {
-                    if (retryCount > retryLimit)
-                        throw new RetryLimitException("Retry file write limit reached.");
+                        throw new RetryLimitException("Retry limit reached.");
                 }
             }
         }
@@ -92,8 +42,15 @@ namespace FileManagement
                 int bytesRead;
                 do
                 {
-                    bytesRead = await ReadRetryAsync(input, buffer);
-                    totalBytesWritten += await WriteRetryAsync(output, buffer, bytesRead);
+                    bytesRead = await RetryAsync(async () => 
+                        await input.ReadAsync(buffer, 0, buffer.Length).ConfigureAwait(false));
+
+                    var bytesToWrite = bytesRead;
+                    totalBytesWritten += await RetryAsync(async () =>
+                    {
+                        await output.WriteAsync(buffer, 0, bytesToWrite).ConfigureAwait(false);
+                        return bytesToWrite;
+                    });
                 } while (bytesRead > 0);
             }
             finally
