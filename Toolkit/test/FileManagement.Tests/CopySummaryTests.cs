7389f4550e284e769c130bae3078d0a1d50de86b
Lopez, Francisco <francisco.lopez2@thermofisher.com>
2019-01-15 12:19:41 +0100

    added initial unit test

---
+++ Toolkit/test/FileManagement.Tests/CopySummaryTests.cs
@@ -0,0 +1,18 @@
+using System;
+using FileManagement.Core;
+using Xunit;
+
+namespace FileManagement.Tests
+{
+    public class CopySummaryTests
+    {
+        [Fact]
+        public void Create_AllOk_ReturnsValidInstance()
+        {
+            //var instance = CopySummary.Create(null, null, 0);
+
+            //Assert.NotNull(instance);
+            Assert.True(true);
+        }
+    }
+}
